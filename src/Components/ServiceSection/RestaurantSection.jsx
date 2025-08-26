1644c260bb8e7ca56be305cdcbd95cd66964964c
SMV_Shivam.dev <03shivam03@gmail.com>
2025-08-27 00:04:35 +0530

    Design hero section layout

--- src/Components/ServiceSection/RestaurantSection.jsx
+++ src/Components/ServiceSection/RestaurantSection.jsx
@@ -73,7 +73,7 @@ const RestaurantSection = () => {
           <div className={`flex`}>
             {items1.map((i) => (
               <a href="">
-                <div className="xl:h-[180px] xl:w-[184px] sm:h-28 sm:w-32 h-20 w-24  ">
+                <div className="xl:h-[180px] xl:w-[184px] sm:h-28 sm:w-32 h-[90px] w-[105px]  ">
                   <img
                     src={SERVICE_BASE_URL + i + ".png"}
                     className="w-full h-full"
@@ -86,7 +86,7 @@ const RestaurantSection = () => {
           <div className="flex ">
             {items2.map((i) => (
               <a href="">
-                <div className="xl:h-[180px] xl:w-[184px] sm:h-28 sm:w-32 h-20 w-24">
+                <div className="xl:h-[180px] xl:w-[184px] sm:h-28 sm:w-32 h-[90px] w-[105px]">
                   <img
                     src={SERVICE_BASE_URL + i + ".png"}
                     className="w-full h-full"
